85708dbf24d8afbdd40cb7b4983e3cad9aca03cf
vishnu <w0834097@myscc.ca>
2024-09-18 20:18:48 +0530

    Assingment is done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {JsonPipe, NgForOf} from "@angular/common";
+import {User} from "./shared/models/user";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,4 +14,14 @@ export class AppComponent {
   title = 'vishnu-sharma-angular-project';
   name = 'Vishnu Sharma';
   hobby= 'music';
+
+  users: User[] = [
+    { id: 1, firstName: 'Ajay', lastName: 'Singh', hobby: 'video editting', isAdmin: true },
+    { id: 2, firstName: 'karan', lastName: 'Arjun', hobby: 'Movies', isAdmin: false },
+    { id: 3, firstName: 'Jai', lastName: 'Vaishist', hobby: 'Cycling' },  // Optional isAdmin
+    { id: 4, firstName: 'Diana', lastName: 'Richard', hobby: 'Gymnastics', isAdmin: true },
+    { id: 5, firstName: 'Chris', lastName: 'Brown', hobby: 'Acting' },       // Optional isAdmin
+    { id: 6, firstName: 'Mathew', lastName: 'haugs', hobby: 'Teaching', isAdmin: true }
+  ];
+
 }
