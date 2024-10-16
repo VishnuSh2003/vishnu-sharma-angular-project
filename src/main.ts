eb6a402e56581c74579710925f8208db5a09a6b1
Vishnu Sharma <W0834097@myscc.ca>
2024-10-17 01:06:40 +0530

    completed first 5 step of assignment-5

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import {provideRouter, Routes} from "@angular/router";
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {FighterListItemComponent} from "./app/fighter-list-item/fighter-list-item.component";
+import {FighterListComponent} from "./app/fighter-list/fighter-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path:'', redirectTo: '/fighters', pathMatch: 'full'},
+  {path:'fighters', component: FighterListComponent},
+  {path:'fighters/:firstName', component: FighterListItemComponent}
+]
+
+bootstrapApplication(AppComponent, {providers: [provideRouter(routes)]
+}). then(r=> console.log('Bootstrap successful'));
