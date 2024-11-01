d95c3eae8dc0099975f1cf79823c1cefa6320d8d
Vishnu Sharma <W0834097@myscc.ca>
2024-11-02 00:40:28 +0530

    corrected the assingnment 5. imported router link.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {JsonPipe, NgForOf} from "@angular/common";
 import {Fighter} from "./shared/models/user";
 import {FighterListComponent} from "./fighter-list/fighter-list.component";
@@ -7,7 +7,7 @@ import {FighterListComponent} from "./fighter-list/fighter-list.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, JsonPipe, FighterListComponent],
+  imports: [RouterOutlet, NgForOf, JsonPipe, FighterListComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
