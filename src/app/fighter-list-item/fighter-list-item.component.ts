1c9e0ab1e9f2aca8233f973427f6b8e8bff41862
Vishnu Sharma <w0834097@myscc.caa>
2024-11-27 00:26:25 +0530

    assignment 8 part 1 done

--- src/app/fighter-list-item/fighter-list-item.component.ts
+++ src/app/fighter-list-item/fighter-list-item.component.ts
@@ -1,12 +1,15 @@
 import {Component, Input} from '@angular/core';
 import {Fighter} from "../shared/models/user";
-import {NgIf} from "@angular/common";
+import {CurrencyPipe, NgIf, PercentPipe, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-fighter-list-item',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    UpperCasePipe,
+    CurrencyPipe,
+    PercentPipe
   ],
   templateUrl: './fighter-list-item.component.html',
   styleUrl: './fighter-list-item.component.css'
