6bc12dd67859862960d592b9ec3da9119891e54e
Vishnu Sharma <w0834097@myscc.caa>
2024-11-27 01:15:02 +0530

    assignment 9 complete

---
+++ src/app/highlight-on-focus.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  standalone: true,
+  selector: '[appHighlightOnFocus]'
+})
+export class HighlightOnFocusDirective {
+  @Input() highlightColor: string = 'lightblue'; // Default highlight color
+  @Input() defaultColor: string = ''; // Optional default color
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener('focus') onFocus() {
+    this.applyHighlight(this.highlightColor);
+  }
+
+  @HostListener('blur') onBlur() {
+    this.applyHighlight(this.defaultColor);
+  }
+
+  private applyHighlight(color: string) {
+    this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', color);
+  }
+}
